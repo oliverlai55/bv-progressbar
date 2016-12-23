a94044c2157fee682ac85ffb4e93f2934b2a75ff
Oliver Lai <oliverlai55@gmail.com>
2016-12-23 14:05:28 -0500

    Update progress

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   var barWidth = $('#progressbar').width();
   var timeCount;
+  var $progressBar = $('#progressbar')
   function startProgress() {
 
     console.log($('#progressbar').width())
@@ -26,7 +27,7 @@ $(document).ready(function() {
 
   function animateWidth(width, speed) {
     // var speed = 10000;
-    $('#progressbar').animate({
+    $progressBar.animate({
       width: width}, {
       duration: speed,
       easing: 'linear',
@@ -36,9 +37,12 @@ $(document).ready(function() {
   // while(barWidth <= 100) {
   //   setInterval(increaseWidth(), 50000);
   // }
-  // console.log(barWidth);
-
-  animateWidth('100px',5000)
-  animateWidth('200px', 2500)
-  animateWidth('300px', 1000)
+  var barStyle = document.getElementById('progressbar').style.width
+  console.log(barStyle);
+  // animateWidth('100px',5000)
+  // animateWidth('200px', 2500)
+  // animateWidth('300px', 1000)
+  // document.getElementById('progressbar').innerHTML = $('#progressbar').width().toString()
 })
+
+// http://www.w3schools.com/w3css/tryit.asp?filename=tryw3css_progressbar_labels_js2
